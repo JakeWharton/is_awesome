b82571f889a6c12804f62f99bb1bca007352c909
JakeWharton <jakewharton@gmail.com>
2009-07-12 00:43:24 -0400

    Changed warning and error lists to pyy ul objects and just add li elements to them directly.

--- is_awesome.py
+++ is_awesome.py
@@ -19,7 +19,7 @@ def MediaInfo2Dict(text):
         if pairs:
             name = pairs.pop(0).replace(' ', '_').lower()
             res[name] = dict(rkeyval.split(pair, 2) for pair in pairs)
-    if 'Encoding settings' in res['video']:
+    if 'video' in res and 'Encoding settings' in res['video']:
         res['video']['Encoding settings'] = dict(pair.split('=', 1) for pair in map(str.strip, filter(None, res['video']['Encoding settings'].split('/'))))
     return res
 
@@ -29,56 +29,52 @@ def is_awesome(text, is_animation):
     
     is_a = True
     is_d = True
-    warnings = []
-    errors = []
+    warnings = ul()
+    errors = ul()
     
-    def warn_d(message):
-        warnings.append(li(strong('DXVA: '), message))
-    def error_a(message, value=None):
-        is_a = False
-        if value:
-            errors.append(li(strong('Awesome: '), '%s Got: %s' % (message, value)))
-        else:
-            errors.append(li(strong('Awesome: '), message))
-    def error_d(message, value=None):
-        is_d = False
-        if value:
-            errors.append(li(strong('DXVA: '), '%s Got: %s' % (message, value)))
-        else:
-            errors.append(li(strong('DXVA: '), message))
+    a = strong('Awesome: ')
+    d = strong('DXVA: ')
     
     is_1080p = (info['video']['Width'] == '1 920 pixels')
     is_720p  = (info['video']['Width'] == '1 280 pixels')
     
     #Check 1: 720p/1080p
     if not is_720p and not is_1080p:
-        error_a('Must be 1920 or 1280 pixels wide.', info['video']['Width'])
+        is_a = False
+        errors += li(a, 'Must be 1920 or 1280 pixels wide. Got: ', info['video']['Width'])
     
     #Check 2: is x264
     if info['video']['Codec ID'] != 'V_MPEG4/ISO/AVC':
-        error_a('Not MPEG4/ISO/AVC (x264) codec.', info['video']['Codec ID'])
+        is_a = False
+        errors += li(a, 'Not MPEG4/ISO/AVC (x264) codec. Got: ', info['video']['Codec ID'])
     
     #Check 3: DTS or AC3 track
     if 'audio' in info:
         a_codec = info['audio']['Format']
         if not a_codec == 'DTS' and not a_codec == 'AC3':
-            error_a('Audio track not DTS or AC3.', info['audio']['Format'])
+            is_a = False
+            errors += li(a, 'Audio track not DTS or AC3. Got: ', info['audio']['Format'])
     elif 'audio_#1' in info:
         a_codec = info['audio_#1']['Format']
         if not a_codec == 'DTS' and not a_codec == 'AC3':
-            error_a('Audio track not DTS or AC3.', info['audio_#1']['Format'])
+            is_a = False
+            errors += li(a, 'Audio track not DTS or AC3. Got: ', info['audio_#1']['Format'])
     else:
-        error_a('No audio tracks.')
+        is_a = False
+        errors += li(a, 'No audio tracks.')
     
     #Check 4: English subtitles
     if 'text' in info:
         if info['text']['Language'] != 'English':
-            error_a('Subtitle track not English.', info['text']['Language'])
+            is_a = False
+            errors += li(a, 'Subtitle track not English. Got: ', info['text']['Language'])
     elif 'text_#1' in info:
         if info['text_#1']['Language'] != 'English':
-            error_a('Subtitle track not English.', info['text_#1']['Language'])
+            is_a = False
+            errors += li(a, 'No English subtitles. Got: ', info['text_#1']['Language'])
     else:
-        error_a('No subtitle tracks.')
+        is_a = False
+        errors += li('No subtitle tracks.')
     
     height = int(info['video']['Height'].replace('pixels', '').replace(' ', '').strip())
     ref = int(info['video']['Encoding settings']['ref'])
@@ -86,93 +82,112 @@ def is_awesome(text, is_animation):
         if height <= 720:
             #Check 6: 3<=ref<=6
             if ref < 3 or ref > 6:
-                error_d('Reference frames not between 3 and 6.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 3 and 6. Got: ', ref)
         elif height <= 864:
             #Check 6: 3<=ref<=5
             if ref < 3 or ref > 5:
-                error_d('Reference frames not between 3 and 5.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 3 and 5. Got: ', ref)
         else:
             #Check 6: 3<=ref<=4
             if ref < 3 or ref > 4:
-                error_d('Reference frames not between 3 and 4.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 3 and 4. Got: ', ref)
     else:
         if height <= 540:
             #Check 6: 5<=ref<=12
             if ref < 5 or ref > 12:
-                error_d('Reference frames not between 5 and 12.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 5 and 12. Got: ', ref)
         elif height <= 588:
             #Check 6: 5<=ref<=11
             if ref < 5 or ref > 11:
-                error_d('Reference frames not between 5 and 11.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 5 and 11. Got: ', ref)
         elif height <= 648:
             #Check 6: 5<=ref<=10
             if ref < 5 or ref > 10:
-                error_d('Reference frames not between 5 and 10.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 5 and 10. Got: ', ref)
         else:
             #Check 6: 5<=ref<=9
             if ref < 5 or ref > 9:
-                error_d('Reference frames not between 5 and 9.', ref)
+                is_d = False
+                errors += li(d, 'Reference frames not between 5 and 9. Got: ', ref)
     
     #Check 5: cabac = 1
     if info['video']['Encoding settings']['cabac'] != '1':
-        error_a(code('cabac')+' must be 1.', info['video']['Encoding settings']['cabac'])
+        is_a = False
+        errors += li(a, code('cabac'), ' must be 1. Got: ', info['video']['Encoding settings']['cabac'])
     
     #Check 7: vbv_maxrate <= 50000
     if 'vbv_maxrate' in info['video']['Encoding settings']:
         if int(info['video']['Encoding settings']['vbv_maxrate']) > 50000:
-            error_d(code('vbv_maxrate'), ' must be less than or equal to 50,000.', info['video']['Encoding settings']['vbv_maxrate'])
+            is_d = False
+            errors += li(d, code('vbv_maxrate'), ' must be less than or equal to 50,000. Got: ', info['video']['Encoding settings']['vbv_maxrate'])
     else:
-        warn_d(code('vbv_maxrate')+' can not be determined. Assuming a value lower than 50,000.')
+        warnings += li(code('vbv_maxrate'), ' can not be determined. Assuming a value lower than 50,000.')
     
     #Check 8: vbv_bufsize <= 50000
     if 'vbv_bufsize' in info['video']['Encoding settings']:
         if int(info['video']['Encoding settings']['vbv_bufsize']) > 50000:
-            error_d(code('vbv_bufsize')+' must be less than or equal to 50,000.', info['video']['Encoding settings']['vbv_bufsize'])
+            is_d = False
+            errors += li(d, code('vbv_bufsize'), ' must be less than or equal to 50,000.', info['video']['Encoding settings']['vbv_bufsize'])
     else:
-        warn_d(code('vbv_bufsize')+' can not be determined. Assuming a value lower than 50,000.')
+        warnings += li(d, code('vbv_bufsize'), ' can not be determined. Assuming a value lower than 50,000.')
     
     #Check 9: analyse = 0x3:0x113
     if info['video']['Encoding settings']['analyse'] != '0x3:0x113':
-        error_d(code('analyse')+' must be 0x3:0x113.', info['video']['Encoding settings']['analyse'])
+        is_d = False
+        errors += li(d, code('analyse'), ' must be 0x3:0x113. Got: ', info['video']['Encoding settings']['analyse'])
     
     #Check 10: rc = crf or 2-pass
     rc = info['video']['Encoding settings']['rc']
     if rc != 'crf' and rc != '2pass':
-        error_a(code('rc')+' must be CRF or 2pass.', info['video']['Encoding settings']['rc'])
+        is_a = False
+        errors += li(a, code('rc'), ' must be CRF or 2pass. Got: ', info['video']['Encoding settings']['rc'])
     
     #Check 11: me_range >= 16
     if int(info['video']['Encoding settings']['me_range']) < 16:
-        error_a(code('me_range')+' must be greater than or equal to 16.', info['video']['Encoding settings']['me_range'])
+        is_a = False
+        errors += li(a, code('me_range'), ' must be greater than or equal to 16. Got: ', info['video']['Encoding settings']['me_range'])
     
     #Check 12: trellis = 1 or 2 or deadzone <= 10
     trellis = info['video']['Encoding settings']['trellis']
     if trellis != '1' and trellis != 2:
         deadzone = map(int, info['video']['Encoding settings']['deadzone'].split(','))
         if deadzone[0] > 10 or deadzone[1] > 10:
-            error_a(code('trellis')+' must be 1 or 2 or ', code('deadzone'), ' values need to be less than 10.', info['video']['Encoding settings']['trellis'] + ' and ' + info['video']['Encoding settings']['deadzone'])
+            is_a = False
+            errors += li(a, code('trellis')+' must be 1 or 2 or ', code('deadzone'), ' values need to be less than 10. Got: ', info['video']['Encoding settings']['trellis'], ' and ', info['video']['Encoding settings']['deadzone'])
     
     #Check 13: bframe >= 3
     if int(info['video']['Encoding settings']['bframes']) < 3:
-        error_a(code('bframes')+' must be greater than or equal to 3.', info['video']['Encoding settings']['bframes'])
+        is_a = False
+        errors += li(a, code('bframes'), ' must be greater than or equal to 3. Got: ', info['video']['Encoding settings']['bframes'])
     
     deblock = int(info['video']['Encoding settings']['deblock'].split(':')[2])
     if is_animation:
         #Check 14: 0<=deblock<=2
         if deblock < 0 or deblock > 2:
-            error_a(code('deblock')+' must be between 0 and 2.', deblock)
+            id_a = False
+            errors += li(a, code('deblock'), ' must be between 0 and 2. Got: ', deblock)
     else:
         #Check 14: -3<=deblock<=-1
         if deblock < -3 or deblock > -1:
-            error_a(code('deblock')+' must be between -3 and -1.', deblock)
+            is_a = False
+            errors += li(a, code('deblock'), ' must be between -3 and -1. Got: ', deblock)
     
     #Check 15: me != dia or hex
     me = info['video']['Encoding settings']['me']
     if me == 'dia' or me == 'hex':
-        error_a(code('me')+' must not be "dia" or "hex".', me)
+        is_a = False
+        errors += li(a, code('me'), ' must not be "dia" or "hex". Got: ', me)
     
     #Check 16: subme >= 7
     if int(info['video']['Encoding settings']['subme']) < 7:
-        error_a(code('subme')+' must be greater than or equal to 7.', info['video']['Encoding settings']['subme'])
+        is_a = False
+        errors += li(a, code('subme'), ' must be greater than or equal to 7. Got: ', info['video']['Encoding settings']['subme'])
     
     #AWESOME!
     return (is_a and is_d, is_d, errors, warnings)
@@ -194,24 +209,20 @@ if 'mediainfo' in web.post:
         (is_a, is_d, errors, warnings) = is_awesome(web.post['mediainfo'], 'is_animation' in web.post)
         
         dxva = div(h1('DXVA'), p('DirectX Video Acceleration (DXVA) is a Microsoft API specification for the Microsoft Windows and Xbox 360 platforms that allows video decoding to be hardware accelerated.'))
-        awsm = div(h1('Awesome'), p('"Awesome" is a standard higher than DXVA. It is used to determine the highest quality encodes by certain individuals. If you are unaware of it, then you most likely do not need to concern yourself with its compliance.'))
+        awsm = div(h1('Awesome'), p('"Awesome" is a standard higher than DXVA. It is used to determine the highest quality encodes by certain individuals. If you are unaware of it, then you most likely do not need to concern yourself with compliance.'))
         
         dxva['class'] = is_d and 'good' or 'bad'
         awsm['class'] = is_a and 'good' or 'bad'
         content += dxva
         content += awsm
         
-        if len(warnings) > 0:
+        if len(warnings.children) > 0:
             content += h3('Warnings')
-            e = content.add(ul())
-            for warning in warnings:
-                e += warning
+            content += warnings
         
-        if not is_a or not is_d:
+        if len(errors.children) > 0
             content += h3('Errors')
-            e = content.add(ul())
-            for error in errors:
-                e += error
+            content += errors
     except (ValueError, KeyError), e:
         content += div(h1('Fatal Error'), p(e.__class__.__name__, ': ', e), _class='bad')
         
