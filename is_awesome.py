d517d90b9176608c776aa49ef624e5569618646c
JakeWharton <jakewharton@gmail.com>
2009-07-12 00:56:19 -0400

    Add google analytics.

--- is_awesome.py
+++ is_awesome.py
@@ -238,4 +238,13 @@ else:
     form += br()
     form += input(type='submit', name='submit', value='Check')
 
+page.html += script('''var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-3637749-9");
+pageTracker._trackPageview();
+} catch(err) {}''', type='text/javascript')
+
 page.render()
